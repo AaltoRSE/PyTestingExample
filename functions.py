7c9e7058af3ea88e3a0146c1ec4ea1c78ac4c9c6
Thomas Pfau <github@thomaspfau.de>
2023-03-13 13:17:58 +0200

    linting with black

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
-def add(a, b):    
+def add(a, b):
     return a + b
+
 
 def subtract(a, b):
     return a + b
 
-def multiply(a,b):
+
+def multiply(a, b):
     return a * b
 
+
 def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit,32),9/5)
+    return multiply(subtract(fahrenheit, 32), 9 / 5)
--- test_functions.py
+++ test_functions.py
@@ -1,16 +1,18 @@
-from functions import add,subtract,multiply
+from functions import add, subtract, multiply
 from functions import convert_fahrenheit_to_celsius as f2c
 import pytest
 
+
 def test_add():
-    assert add(2,3) == 5
-    assert add('space','ship') == 'spaceship'
+    assert add(2, 3) == 5
+    assert add("space", "ship") == "spaceship"
 
-#def test_subtract():
+
+# def test_subtract():
 #    assert subtract(3,2) == 1
-    
 
-#def test_convert_fahrenheit_to_celsius():
+
+# def test_convert_fahrenheit_to_celsius():
 #    assert f2c(32) == 0
 #    assert f2c(122) == pytest.approx(50)
 #    with pytest.raises(AssertionError):
