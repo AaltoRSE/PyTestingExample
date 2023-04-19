aec4037f09323f9154d810894bceed7d0158e8eb
Mira Salmensaari <mira.salmensaari@aalto.fi>
2023-04-19 13:18:36 +0300

    Added a test for multiply function

--- test_functions.py
+++ test_functions.py
@@ -11,6 +11,8 @@ def test_add():
 def test_subtract():
     assert subtract(3,2) == 1
 
+def test_multiply():
+    assert multiply(3,2) == 6
 
 # def test_convert_fahrenheit_to_celsius():
 #    assert f2c(32) == 0
